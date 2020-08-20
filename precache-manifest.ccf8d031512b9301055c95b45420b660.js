930f113c57d36fa850623da3550196a7d1631cac
= <2041778+vinicius191@users.noreply.github.com>
2020-08-20 21:29:03 +1000

    Updates

---
+++ precache-manifest.ccf8d031512b9301055c95b45420b660.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5eb8c9c23666e3833c9f8404e612da",
+    "url": "/react_weather/index.html"
+  },
+  {
+    "revision": "6ae9113b8c835202e54e",
+    "url": "/react_weather/static/css/main.e4a502b5.chunk.css"
+  },
+  {
+    "revision": "430c5c8efc7422525b7c",
+    "url": "/react_weather/static/js/2.7d8cb51c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_weather/static/js/2.7d8cb51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae9113b8c835202e54e",
+    "url": "/react_weather/static/js/main.007c1198.chunk.js"
+  },
+  {
+    "revision": "a679dbfe06bbfac4f494",
+    "url": "/react_weather/static/js/runtime-main.8352dfb7.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/react_weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/react_weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
